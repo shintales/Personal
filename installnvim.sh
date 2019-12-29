15f2079adeea1534e406e5fb903217d907dc3da5
tajali016 <tta16@my.fsu.edu>
2019-12-29 00:47:03 -0500

    Update installnvim.sh

--- installnvim.sh
+++ installnvim.sh
@@ -3,3 +3,4 @@ sudo cp nvim.appimage /usr/bin/nvim
 chmod u+x /usr/bin/nvim
 mkdir -p ~/.config/nvim
 touch ~/.config/nvim/init.vim
+curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
