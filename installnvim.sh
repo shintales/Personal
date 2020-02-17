66e33f15a4174e58aab847b3b688384ca91a9e16
Taj Ali <ta19815@gmail.com>
2020-02-16 19:43:20 -0500

    updated nvim install

--- installnvim.sh
+++ installnvim.sh
@@ -1,6 +1,6 @@
 curl -LO https://github.com/neovim/neovim/releases/download/stable/nvim.appimage
 sudo cp nvim.appimage /usr/bin/nvim
-chmod u+x /usr/bin/nvim
+chmod a+rx /usr/bin/nvim
 mkdir -p ~/.config/nvim
 touch ~/.config/nvim/init.vim
 curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
